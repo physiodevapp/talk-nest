facfe7f8682dbd620eb7b758ec65f05b310a7c76
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-12-13 10:07:14 +0100

    feat: add refresh token

--- server/controlllers/user.js
+++ server/controlllers/user.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken'
 import { UserModel } from '../models/user.js'
-import { JWT_SECRET_KEY, NODE_ENV } from '../../config.js'
+import { JWT_SECRET_KEY, REFRESH_SECRET_KEY, ACCESS_TOKEN_EXPIRATION, REFRESH_TOKEN_EXPIRATION, NODE_ENV, REFERSH_SECRET_KEY } from '../../config.js'
+
+const generateAccessToken = (user) => jwt.sign(user, JWT_SECRET_KEY, { expiresIn: ACCESS_TOKEN_EXPIRATION })
+const generateRefreshToken = (user) => jwt.sign(user, REFRESH_SECRET_KEY, { expiresIn: REFRESH_TOKEN_EXPIRATION })
 
 export class UserController {
   static register = async (req, res) => {
@@ -20,15 +23,16 @@ export class UserController {
       })
 
       if (user) {
-        const token = jwt.sign(user, JWT_SECRET_KEY, { expiresIn: '1h' })
+        const accessToken = generateAccessToken({ id: user.id, username: user.username })
+        const refreshToken = generateRefreshToken({ id: user.id, username: user.username })
 
         res
-          .cookie('access_token', token, {
+          .cookie('access_token', refreshToken, {
             httpOnly: true,
             secure: NODE_ENV === 'production',
             sameSite: 'strict'
           })
-          .redirect('/lobby')
+          .json({ accessToken })
       } else {
         res.status(401).redirect('/access?formType=login')
       }
@@ -39,9 +43,27 @@ export class UserController {
     }
   }
 
+  static refresh = async (req, res) => {
+    const refreshToken = req.cookies.refresh_token
+
+    if (!refreshToken) {
+      return res.status(403).send('Token required')
+    }
+
+    try {
+      const user = jwt.verify(refreshToken, REFERSH_SECRET_KEY)
+
+      const newAccessToken = generateAccessToken({ id: user.id, username: user.username })
+
+      res.json({ accessToken: newAccessToken })
+    } catch (error) {
+      throw new Error('Invalid token')
+    }
+  }
+
   static logout = async (req, res) => {
     res
-      .clearCookie('access_token', {
+      .clearCookie('refresh_token', {
         httpOnly: true,
         secure: NODE_ENV === 'production',
         sameSite: 'strict'
