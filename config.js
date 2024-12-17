d6199bf1ed9ba61cbda792bc62d663e195c217df
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-12-17 11:41:38 +0100

    chore: add env variables

--- config.js
+++ config.js
@@ -4,6 +4,11 @@ export const {
   REFRESH_SECRET_KEY,
   ACCESS_TOKEN_EXPIRATION,
   REFRESH_TOKEN_EXPIRATION,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
   NODE_ENV
 } = process.env
 export const JWT_SALT = parseInt(process.env.JWT_SALT, 10)
